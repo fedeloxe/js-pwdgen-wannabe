e1a2c60d76ebddeba96d42f1bc91e7869badd2ea
fedeloxe <loxeee@gmail.com>
2022-11-24 17:47:57 +0100

    script done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//prompt nome 
+let name_user = prompt('inserisci il tuo nome')
+
+//prompt cognome 
+
+let last_name_user = prompt ('inserisci il tuo cognome')
+
+//prompt colore
+
+let color_user = prompt ('inserisci il tuo colore preferito') 
+
+//valore 21 
+
+let pass_21 = 21
+
+//sommo i valori e recupero la destinazione 
+
+document.getElementById("password").innerHTML = name_user + last_name_user + color_user + pass_21;
+
